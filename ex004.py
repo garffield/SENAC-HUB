cb8f670de242b17001762499c5f546c05a44e3d9
garffield <gabrielfranklinbarcellos@gmail.com>
2024-05-20 09:42:24 -0300

    Rename

---
+++ ex004.py
@@ -0,0 +1,10 @@
+country = ['Alemanha', 'Itália', 'Japão']
+
+country.append ("Brasil")
+print(country)
+
+country.insert (1, 'Brasil')
+print(country)
+
+country.insert (3, 'Brasil')
+print (country)
