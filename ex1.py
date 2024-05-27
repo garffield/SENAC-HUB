db493c29be6a1a62adb041e7d1a5389a54debcb8
garffield <gabrielfranklinbarcellos@gmail.com>
2024-05-27 12:04:36 -0300

    .

--- ex1.py
+++
@@ -1,2 +0,0 @@
-X = int(input("Pense em um numero, irei adivinhar qual numero esta pensando... "))
-print ("O numero pensado foi...", X, " !!!")
--- ex4.py
+++
@@ -1,8 +0,0 @@
-n1 = float(input("Informe sua nota 1: "))
-n2 = float(input("Informe sua nota 2: "))
-n3 = float(input("Informe sua nota 3: "))
-n4 = float(input("Informe sua nota 4: "))
-
-media = (n1 + n2 + n3 + n4) / 4
-
-print ("A média é: %.2f" %(media))
