ce0010b27cbd154ac65ea57074dc25b3d9991c79
garffield <gabrielfranklinbarcellos@gmail.com>
2024-05-27 08:41:25 -0300

    .

--- README.md
+++
@@ -1,2 +0,0 @@
-# SENAC-HUB
-Aulas e Projetos feitos durante o curso VOUCHER DESENVOLVEDOR no SENAC - MS
--- ex02.py
+++
@@ -1,4 +0,0 @@
-x1 = input("Digite algo: ")
-x2 = input("Digite algo: ")
-
-print(f'No primeiro texto ha {len(x1)} letras \nNo segundo texto ha {len(x2)} letras')
