6be97951ef9123767973ba9530a405b26c89f7fd
garffield <gabrielfranklinbarcellos@gmail.com>
2024-08-06 10:20:27 -0300

    .

--- ex005.py
+++
@@ -1,8 +0,0 @@
-lista = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-print(lista[2:10])
-print(lista[9:11])
-print(lista[0:11:2])
-print(lista[1:10:2])
-lista.reverse()
-print(lista)
--- ex19.py
+++
@@ -1,7 +0,0 @@
-n1 = float(input("Informe um numero: "))
-n2 = float(input("Informe um numero: "))
-
-if n1 > n2:
-    print (n1, " é maior.")
-else: 
-    print (n2, " é maior.")
