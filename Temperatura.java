1e3102c18f55d082a5b6f496b235f0266303ba29
garffield <gabrielfranklinbarcellos@gmail.com>
2024-08-07 13:52:40 -0300

    .

---
+++ Temperatura.java
@@ -0,0 +1,15 @@
+public class Temperatura {
+    public static void main(String[] args) {
+        double idade = 22.60;
+        if (idade == 22.60) {
+            System.out.println("Igual");}
+
+        else if (idade > 20.60) {
+            System.err.println("Maior");} 
+        
+        else {
+            System.out.println("Menor");
+        }
+
+        }
+    }
---
+++ ex.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int x;
+    x = 1 / 2-;
+    printf("%i\n", x);
+    
+    return 0;
+}
